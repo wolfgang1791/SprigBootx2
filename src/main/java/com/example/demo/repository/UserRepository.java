99b05639f9e62d36a15670922ab44cc35db5c806
Jonathan <jonathanperalta1893@gmail.com>
2018-10-05 13:24:31 -0500

    repository

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entity.User;
+
+@Repository("userRepository")
+public interface UserRepository extends JpaRepository<User,Serializable> {
+
+	public abstract User findbyUsername(String username);
+	
+}
